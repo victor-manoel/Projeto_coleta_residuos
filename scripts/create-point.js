982083501b7d0c647645788c16483e7e225d8956
Victor Manoel <victormanoel7895123@hotmail.com>
2020-06-15 23:24:51 -0300

    update javascript

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -15,8 +15,23 @@
 
     populateUFs()
 
+    function getCities(event){
+        const citySelect = document.querySelector("select[name=city]")
+
+        const ufValue = event.target.value 
+
+        const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+        fetch(url)
+        .then(res => res.json())
+        .then( cities => {
+
+            for(const city of cities){
+                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            }
+        })
+    }
+
     document
         .querySelector("select[name=uf]")
-        .addEventListener("change", () => {
-        console.log("mudei")
-    }) 
+        .addEventListener("change", getCities)

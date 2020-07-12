cb5102d026abeecdac97052054d00f73cd6947bc
Victor Manoel <victormanoel7895123@hotmail.com>
2020-07-11 22:08:09 -0300

    update html

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const buttonSearch = document.querySelector("#page-home main a")
-const modal = documentquerySelector("#modal")
-const close = documentquerySelector("#modal .header a")
+const modal = document.querySelector("#modal")
+const close = document.querySelector("#modal .header a")
 
 buttonSearch.addEventListener("click", () => {
     modal.classList.remove("hide")
